43b6cde1b2aa93f3035cc568e67227aabbe04b19
Hector Ubiera <hubiera@3in1media.com>
2025-08-03 16:02:58 -0400

    output

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/hectorubiera/Documents/GitHub/alpha-sluggers/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/53890c8af469a830-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
